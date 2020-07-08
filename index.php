a750deafef4f8a740a27a47352d36a2203a638ea
Aleksander Shumeiko <music.dll@mail.ru>
2020-07-08 09:57:03 +0300

    Added styles for header. Header and footer separated

--- index.php
+++ index.php
@@ -1,34 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Youtube Clone</title>
-    <link rel="stylesheet" href="/assets/css/style.css">
-</head>
-<body>
-
-    <div id="pageContainer">
-
-        <div id="mastHeadContainer">
-            <button class="navShowHide">Click Me!</button>
-        </div>
-
-        <div id="sideNavContainer" style="display: none;">
-
-        </div>
-
-        <div id="mainSectionContainer">
-
-            <div id="mainContentContainer">
-                Hello Everyone!
-            </div>
-
-        </div>
-
-    </div>
-    <script src="assets/js/scripts.js"></script>
-</body>
-</html>
+<?php  require_once 'includes/header.php'; ?>
+            Hello Everyone!
+<?php require_once 'includes/footer.php'; ?>
